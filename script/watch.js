7bc3d97dfe8fff124a1ed3d1e2dd69a9f4d1744b
David Kamholz <lautgesetz@gmail.com>
2017-02-25 23:05:30 -0800

    do an initial make in watch.js

--- script/watch.js
+++ script/watch.js
@@ -3,10 +3,12 @@
 var chokidar = require('chokidar');
 var spawn = require('child_process').spawn;
 
-chokidar.watch(['bootstrap','js','style.styl', 'templates'])
-.on('change', function (f) {
+make();
+
+chokidar.watch(['bootstrap','js','style.styl','templates'])
+.on('change', make)
+.on('error', function (err) { console.log(err) });
+
+function make() {
   spawn('npm', ['run','make'], { stdio: 'inherit' });
-})
-.on('error', function (err) {
-  console.log(err);
-});
+}
