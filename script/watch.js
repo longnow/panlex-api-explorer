42d7e2b9c19204037e121deaf48890cb881da4cb
David Kamholz <lautgesetz@gmail.com>
2017-02-26 21:28:43 -0800

    improve watch.js output

--- script/watch.js
+++ script/watch.js
@@ -10,5 +10,18 @@ chokidar.watch(['bootstrap','js','style.styl','templates'])
 .on('error', function (err) { console.log(err) });
 
 function make() {
-  spawn('npm', ['run','make'], { stdio: 'inherit' });
+  console.log(new Date() + ': make');
+
+  var error = false;
+
+  var proc = spawn('npm', ['run','make'], { stdio: ['ignore','ignore','pipe'] });
+
+  proc.stderr.on('data', function (chunk) {
+    process.stderr.write(chunk, 'utf8');
+    error = true;
+  });
+
+  proc.on('close', function (code) {
+    if (error) console.log('');
+  });
 }
